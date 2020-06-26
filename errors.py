18ef635db480108f1ed3ee7c7155643bbd612699
HRS0986 <heshanhfernando@gmail.com>
2020-06-26 12:39:45 +0530

    WeekPasswordException error codes added

--- errors.py
+++ errors.py
@@ -10,23 +10,25 @@ class WrongPasswordError(Exception):
         super(WrongPasswordError).__init__(error)
         self.error_code = 21
 
-class WeekPasswordError(Exception):
-    def __init__(self, error):
-        super(WeekPasswordError, self).__init__(error)
-        self.error_code = 22
-
 class InvalidCommandError(Exception):
     def __init__(self, error):
         super(InvalidCommandError, self).__init__(error)
-        self.error_code = 23
+        self.error_code = 22
 
 class RecordNotFoundError(Exception):
     def __init__(self, error):
         super(RecordNotFoundError, self).__init__(error)
-        self.error_code = 24
+        self.error_code = 23
 
 class NotAlphaError(Exception):
     def __init__(self, error):
         super(NotAlphaError, self).__init__(error)
-        self.error_code = 25
+        self.error_code = 24
 
+class WeekPasswordError(Exception):
+    def __init__(self, error, error_code):
+        super(WeekPasswordError, self).__init__(error)
+        self.error_code = error_code
+        # 25 - No Digit
+        # 26 - No Letter
+        # 27 - Too Short
